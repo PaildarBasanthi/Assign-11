628909c3590e7aa7b81eb84da4458cbb0d0a1283
PaildarBasanthi <basanthisrec@gmail.com>
2023-07-20 16:11:54 +0530

    first Commit

---
+++ LargeDataCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_11
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<object> data; // The internal data structure to hold a large amount of data.
+
+        public LargeDataCollection(IEnumerable<object> initialData)
+        {
+            // Initialize the internal data structure and populate it with initial data.
+            data = new List<object>(initialData);
+        }
+
+        public void Add(object item)
+        {
+            // Add an element to the collection.
+            data.Add(item);
+        }
+
+        public bool Remove(object item)
+        {
+            // Remove an element from the collection.
+            return data.Remove(item);
+        }
+
+        public object GetElement(int index)
+        {
+            // Get an element from the collection based on the index.
+            if (index >= 0 && index < data.Count)
+            {
+                return data[index];
+            }
+            return null; // Or throw an exception if you prefer.
+        }
+
+        public void Dispose()
+        {
+            // Implement the IDisposable pattern to release unmanaged resources.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Release managed resources.
+                data.Clear();
+                data = null;
+            }
+            // Release any other unmanaged resources here.
+        }
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize LargeDataCollection with initial data
+
+            // Create an instance of LargeDataCollection and populate it with initial data.
+            var initialData = new object[] { 1, 2, 3, "hello", "world" };
+            var largeDataCollection = new LargeDataCollection(initialData);
+
+            // Add more elements.
+            largeDataCollection.Add(4);
+            largeDataCollection.Add("extra data");
+
+            // Access elements by index.
+            object element = largeDataCollection.GetElement(2);
+            Console.WriteLine("Element at index 2: " + element);
+
+            // Remove an element.
+            bool isRemoved = largeDataCollection.Remove(3);
+            Console.WriteLine("Element removed: " + isRemoved);
+
+            // Explicitly call Dispose to release resources and free up memory.
+            largeDataCollection.Dispose();
+        }
+    }
+}
